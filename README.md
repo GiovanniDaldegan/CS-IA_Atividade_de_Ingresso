1fa811a777e6617cb81ac73018e25cbf1b011635
Giovanni Daldegan <giovannidaldegani@gmail.com>
2026-05-24 00:41:51 -0300

    feat: estrutura básica dos arquivos

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # CS-IA_Atividade_Trainee
 Atividade introdutória do Grupo de Estudos de IA da CS - Ramo IEEE UnB
+
+## Agente
+
+## Instalação de dependências
+- python3.10+
+
+## Outros requisitos
+- chave própria do usuário
+
+## Uso da ferramenta
+
---
+++ agent.py
@@ -0,0 +1,16 @@
+# import <pacote modelo>
+
+# obtenção da chave em .env
+# instanciação do cliente
+
+# leitura do system_prompt
+
+# criação do histórico (system_prompt)
+
+# execução do chat
+    # mensagem inicial
+    # loop de execução
+    # gravação da entrada do usuário
+    # repasse pra API da LLM
+    # repasse da resposta do modelo para usuário
+    # gravação da resposta
